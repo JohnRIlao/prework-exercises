0a6357c0b6472e628db28add570aefe7965364d6
JohnRIlao <johnilao@jmiguam.com>
2026-03-12 16:04:56 +1000

    Create 111.rb, note only Q1

---
+++ 111.rb
@@ -0,0 +1,8 @@
+# Question 1
+# Write code to print out the numbers 4, 30, 12, 983 on separate lines
+# For this exercise, you are required to use +, -, *, / at least once
+
+puts 2 + 2
+puts 984 - 1
+puts 3 * 4
+puts 90 / 3
