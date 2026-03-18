945c369d69dc9212b9498a3a9271dc3853a75f42
JohnRIlao <johnilao@jmiguam.com>
2026-03-18 21:35:11 +1000

    Update 111.rb

--- 111.rb
+++ 111.rb
@@ -6,15 +6,3 @@ puts 2 * 2
 puts 90 / 3
 puts 3 * 4
 puts 984 - 1
-
-# QUESTION 2
-# Write code using addition, subtraction, multiplication, or division with numbers that causes the computer to crash.
-# Add a comment with a copy of the error message.
-# Add a comment that describes the error message in your own words.
-
-puts 2 a 2 
-puts 2 * 2 
-90 puts
-puts 12
-# rb will not understand x or number first or P
-
