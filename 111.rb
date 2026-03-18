fead4eb961532944149c3c9ac165955d07407078
JohnRIlao <johnilao@jmiguam.com>
2026-03-18 20:22:17 +1000

    Update 111.rb

--- 111.rb
+++ 111.rb
@@ -1,8 +1,19 @@
-# Question 1
-# Write code to print out the numbers 4, 30, 12, 983 on separate lines
-# For this exercise, you are required to use +, -, *, / at least once
+# QUESTION 1
+# Write code to print out the numbers 4, 30, 12, 983 on separate lines. 
+# For this exercise, you are required to use addition, subtraction, multiplication, and division at least once.
 
-puts 2 + 2
-puts 984 - 1
-puts 3 * 4
+puts 2 * 2
 puts 90 / 3
+puts 3 * 4
+puts 984 - 1
+
+# QUESTION 2
+# Write code using addition, subtraction, multiplication, or division with numbers that causes the computer to crash.
+# Add a comment with a copy of the error message.
+# Add a comment that describes the error message in your own words.
+
+puts 2 x 2 
+puts 2 * 2 
+90 puts
+Puts 12
+# rb will not understand x or number first or P
